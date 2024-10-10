9ea60ba6d77e803ac55e036872257fbda3782554
nagosu <nozick1021@gmail.com>
2024-10-10 20:22:50 +0900

    정산관리 탭 퍼블리싱

--- src/pages/SettlementCompanyManagement/SettlementCompanyManagement.tsx
+++ src/pages/SettlementCompanyManagement/SettlementCompanyManagement.tsx
@@ -1,9 +1,15 @@
+import SettlementCompanyManagementContent from "../../components/content/SettlementCompanyManagementContent";
+import MainHeader from "../../components/header/MainHeader";
 import SideNav from "../../components/sidenav/SideNav";
 
 function SettlementCompanyManagement() {
   return (
     <>
       <SideNav />
+      <div className="w-content flex h-screen flex-col">
+        <MainHeader />
+        <SettlementCompanyManagementContent />
+      </div>
     </>
   );
 }
