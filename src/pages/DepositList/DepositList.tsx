8033dc50065a718056079679ed33590a7869adc4
nagosu <nozick1021@gmail.com>
2024-10-11 18:00:15 +0900

    입금 리스트 페이지 퍼블리싱

--- src/pages/DepositList/DepositList.tsx
+++ src/pages/DepositList/DepositList.tsx
@@ -1,9 +1,15 @@
+import DepositListContent from "../../components/content/DepositListContent";
+import MainHeader from "../../components/header/MainHeader";
 import SideNav from "../../components/sidenav/SideNav";
 
 function DepositList() {
   return (
     <>
       <SideNav />
+      <div className="flex h-screen w-content flex-col">
+        <MainHeader />
+        <DepositListContent />
+      </div>
     </>
   );
 }
