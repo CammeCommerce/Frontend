103f721494df801e7723953bc103528d5d8345b4
nagosu <nozick1021@gmail.com>
2024-10-11 18:37:00 +0900

    온라인 리스트 페이지 퍼블리싱

--- src/pages/OnlineList/OnlineList.tsx
+++ src/pages/OnlineList/OnlineList.tsx
@@ -1,9 +1,15 @@
+import OnlineListContent from "../../components/content/OnlineListContent";
+import MainHeader from "../../components/header/MainHeader";
 import SideNav from "../../components/sidenav/SideNav";
 
 function OnlineList() {
   return (
     <>
       <SideNav />
+      <div className="flex h-screen w-content flex-col">
+        <MainHeader />
+        <OnlineListContent />
+      </div>
     </>
   );
 }
