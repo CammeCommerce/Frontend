d1e419106c6694dc7dc552f0b4a87bcb7ac82654
nagosu <nozick1021@gmail.com>
2024-10-11 18:05:30 +0900

    입금 매칭리스트 페이지 퍼블리싱

--- src/pages/DepositMatchingList/DepositMatchingList.tsx
+++ src/pages/DepositMatchingList/DepositMatchingList.tsx
@@ -1,9 +1,15 @@
+import DepositMatchingListContent from "../../components/content/DepositMatchingListContent";
+import MainHeader from "../../components/header/MainHeader";
 import SideNav from "../../components/sidenav/SideNav";
 
 function DepositMatchingList() {
   return (
     <>
       <SideNav />
+      <div className="flex h-screen w-content flex-col">
+        <MainHeader />
+        <DepositMatchingListContent />
+      </div>
     </>
   );
 }
