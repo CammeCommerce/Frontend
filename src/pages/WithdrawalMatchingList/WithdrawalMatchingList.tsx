a0ba899c4e6497b962090bbe3cd4b4a2d82e104d
nagosu <nozick1021@gmail.com>
2024-10-11 18:16:16 +0900

    출금 매칭리스트 페이지 퍼블리싱

--- src/pages/WithdrawalMatchingList/WithdrawalMatchingList.tsx
+++ src/pages/WithdrawalMatchingList/WithdrawalMatchingList.tsx
@@ -1,9 +1,15 @@
+import WithdrawalMatchingListContent from "../../components/content/WithdrawalMatchingListContent";
+import MainHeader from "../../components/header/MainHeader";
 import SideNav from "../../components/sidenav/SideNav";
 
 function WithdrawalMatchingList() {
   return (
     <>
       <SideNav />
+      <div className="flex h-screen w-content flex-col">
+        <MainHeader />
+        <WithdrawalMatchingListContent />
+      </div>
     </>
   );
 }
